be8c2539045203f20c18d67bbca432ac8ada4398
faridhaavasi <faridhavasi777@gmail.com>
2025-04-28 18:55:30 +0330

    Fix import statement for database settings in database configuration

--- app/core/config/database.py
+++ app/core/config/database.py
@@ -1,9 +1,9 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import config
+from app.core.config.config import settings
 
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
