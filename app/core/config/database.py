0d3b178dec3c684b867c4798de10946276752a86
faridhaavasi <faridhavasi777@gmail.com>
2025-05-01 18:52:20 +0330

    Fix typo in TaskModel for automatic_increment attribute

--- app/core/config/database.py
+++ app/core/config/database.py
@@ -1,7 +1,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config.config import settings
+from core.config.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String,func 
+from sqlalchemy import Column, Integer, String, Text,Boolean, func 
 
 from core.config.database import Base
 
 class TaskModel(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, outomatic_increment=True)
     title = Column(String(255), nullable=False)
-    description = Column(String(255), nullable=False)
+    description = Column(Text(500), nullable=True)
+    is_completed = Column(Boolean, default=False)
     created_at = Column(String(50), default=func.now())
     updated_at = Column(String(50), default=func.now(), onupdate=func.now())
 
