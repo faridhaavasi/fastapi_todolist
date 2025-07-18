c850afd558549edf87dd3cd1c194447261d5b5ef
faridhaavasi <faridhavasi777@gmail.com>
2025-07-18 15:20:04 +0330

    restore sturcture

--- core/database.py
+++ core/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from config import settings
+from core.config import settings
+
 engine = create_engine(
     settings.SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False}  # Only for SQLite
+    connect_args={"check_same_thread": False} 
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
