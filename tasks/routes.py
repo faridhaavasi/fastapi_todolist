655408991ff9f6bbb40b5aa65a9bb4434b9f83bf
faridhaavasi <faridhavasi777@gmail.com>
2025-07-27 12:11:06 +0330

    remove prefix in tasks api design

--- tasks/routes.py
+++ tasks/routes.py
@@ -7,7 +7,7 @@ from core.database import get_db
 from typing import List
 
 
-router = APIRouter(tags=["tasks"], prefix='tasks/')
+router = APIRouter(tags=["tasks"])
 
 
 @router.get("/tasks", response_model=List[TaskResponseSchema])
