470c4dcb954f8301940dc2fbcec722f0867c9f27
faridhaavasi <faridhavasi777@gmail.com>
2025-07-27 12:54:36 +0330

    test basic auth in fastapi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from tasks.routes import router as tasks_router
 from users.routes import router as user_router
+from users.models import UserModel as User
 tags_metadata = [
     {
         "name": "Tasks",
@@ -9,6 +10,18 @@ tags_metadata = [
 ]
 
 app = FastAPI(openapi_tags=tags_metadata)
+from auth.bisic_auth import get_authenticated_user
+
+
+@app.post('publice')
+async def publice():
+    return {}
+
+@app.post('private')
+async def private(current_user: User = Depends(get_authenticated_user)):
+    return {}
+
+
 
 # include the router
 app.include_router(tasks_router)
