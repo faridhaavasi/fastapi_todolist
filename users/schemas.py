fbd4c2c66e8d1db36a65fa7885661ecce5e1cd53
faridhaavasi <faridhavasi777@gmail.com>
2025-07-30 16:22:35 +0330

    auth jwt

--- users/schemas.py
+++ users/schemas.py
@@ -24,3 +24,7 @@ class UserRegisterSchema(BaseModel):
         if not (password_confirm == validation.data.get("password")):
             raise ValueError("passwords doesnt match")
         return password_confirm
+
+
+class UserRefreshTokenSchema(BaseModel):
+    token: str = Field(..., description="refresh token of the user")
