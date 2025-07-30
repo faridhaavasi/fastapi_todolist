a310336c2ed119339eea81fa61bb804d9326fdf3
faridhaavasi <faridhavasi777@gmail.com>
2025-07-30 13:40:09 +0330

    add JWT_SECRET_KEY in config file

--- core/config.py
+++ core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str
+    JWT_SECRET_KEY : str = 'test'
 
     class Config:
         env_file = ".env"
